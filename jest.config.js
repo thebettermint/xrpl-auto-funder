1404d625b69fab0feb931b25990b72c44d601e2f
interc0der <intercoder@whirled.io>
2022-06-22 16:44:48 -0700

    Create: initalized project dev config files

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  transform: { '^.+\\.ts?$': 'ts-jest' },
+  testEnvironment: 'node',
+  testRegex: '/tests/.*\\.(test|spec)?\\.(ts|tsx)$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  verbose: true,
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.json',
+    },
+  },
+};
