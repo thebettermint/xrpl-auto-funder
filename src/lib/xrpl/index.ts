a4015124ef4ca25cd3cef06d678831b3d65aae02
interc0der <intercoder@whirled.io>
2022-06-27 18:36:54 -0700

    Update: import/export funding functions for import

---
+++ src/lib/xrpl/index.ts
@@ -0,0 +1,9 @@
+import * as client from './client';
+import { payment } from './payment';
+import { checkBalance } from './checkBalance';
+
+export default {
+  client,
+  payment,
+  checkBalance,
+};
